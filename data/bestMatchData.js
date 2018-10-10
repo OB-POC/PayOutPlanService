2b6280a3441c5d013edd92ca027b36b4ab42c625
Prithvi Raj Vuppalapati <prithvirajvuppalapati@Prithvis-MacBook-Pro.local>
2018-10-10 18:31:46 +0530

    data changed

--- data/bestMatchData.js
+++ data/bestMatchData.js
@@ -4,13 +4,16 @@ module.exports = {
             username: 'alice',
             password: 'wonderland',
             UIN: 'UIN-xxxx',
-            totalAvailableBalance: '4,600',
+            bankleftOut: 'Barclays',
+            typeLeftOut: 'debit',
+            totalAvailableBalance: '4,800',
+            aerLeftOut: '0.25',
             creditDebitMatch: [
                 {
                     bankName: 'Barclays',
                     accountType: 'M',
                     accountTitle: '2 Years Fixed Reward',
-                    totalBalanceDue: '2,000',
+                    totalBalanceDue: '800',
                     clearedTotalDue: true,
                     interestRate: 3.3,
                     totalOutstandingBalance: 0,
@@ -19,33 +22,44 @@ module.exports = {
                             "bankName": "HSBC",
                             "accountType": "PCA",
                             "accountTitle": "HSBC Advance bank Account",
-                            "contributingAmount": '2,000',
+                            "contributingAmount": '800',
                             "totalAvailableBalance": '2,600',
                             "beforeContributingBalance": '2,600',
-                            "afterContributingBalance": '600',
+                            "afterContributingBalance": '1,800',
                             "interestRate": 0.1
                         }
                     ]
                 },
                 {
-                    bankName: 'JPMC',
+                    bankName: 'NatWest',
                     accountType: 'CC',
-                    accountTitle: 'Freedom Unlimited',
+                    accountTitle: 'Reward Black Credit Card',
                     totalBalanceDue: '1,800',
                     clearedTotalDue: true,
-                    apr: 25.49,
+                    apr: 37.10,
                     totalOutstandingBalance: 0,
                     senders: [
                         {
                             "bankName": "HSBC",
                             "accountType": "PCA",
                             "accountTitle": "HSBC Advance bank Account",
-                            "contributingAmount": 600,
-                            "totalAvailableBalance": '2600',
-                            "beforeContributingBalance": 600,
+                            "contributingAmount": '1,800',
+                            "totalAvailableBalance": '2,600',
+                            "beforeContributingBalance": '1,800',
                             "afterContributingBalance": 0,
                             "interestRate": 0.1
-                        },
+                        }
+                    ]
+                },
+                {
+                    bankName: 'RBS',
+                    accountType: 'CC',
+                    accountTitle: 'Unlimited Credit Card',
+                    totalBalanceDue: 1200,
+                    clearedTotalDue: true,
+                    apr: 19.94,
+                    totalOutstandingBalance: 0,
+                    senders: [
                         {
                             "bankName": "Halifax",
                             "accountType": "SB",
@@ -54,7 +68,7 @@ module.exports = {
                             "totalAvailableBalance": '1,000',
                             "beforeContributingBalance": '1,000',
                             "afterContributingBalance": 0,
-                            "interestRate": 0.2
+                            "interestRate": 0.2 
                         },
                         {
                             "bankName": "Barclays",
@@ -66,27 +80,6 @@ module.exports = {
                             "afterContributingBalance": '4,800',
                             "interestRate": 0.25
                         }
-                    ]
-                },
-                {
-                    bankName: 'RBS',
-                    accountType: 'CC',
-                    accountTitle: 'Unlimited Credit Card',
-                    totalBalanceDue: 200,
-                    clearedTotalDue: true,
-                    apr: 19.94,
-                    totalOutstandingBalance: 0,
-                    senders: [
-                        {
-                            "bankName": "Barclays",
-                            "accountType": "SB",
-                            "accountTitle": "Easy Saver",
-                            "contributingAmount": 200,
-                            "totalAvailableBalance": '5,000',
-                            "beforeContributingBalance": '4,800',
-                            "afterContributingBalance": '4,600',
-                            "interestRate": 0.25
-                        }
                     ] 
                 }
             ] 
@@ -95,13 +88,16 @@ module.exports = {
             username: 'john',
             password: 'doe',
             UIN: 'UIN-xxxxx',
-            totalAvailableBalance: '-1,600',
+            typeLeftOut: 'credit',
+            bankLeftOut: 'NatWest',
+            totalAvailableBalance: '-900',
+            aprLeftOut: '23.70',
             creditDebitMatch: [
                 {
                     bankName: 'Barclays',
                     accountType: 'M',
                     accountTitle: '2 Years Fixed Reward',
-                    totalBalanceDue: '2,000',
+                    totalBalanceDue: '800',
                     clearedTotalDue: true,
                     interestRate: 3.3,
                     totalOutstandingBalance: 0,
@@ -110,73 +106,73 @@ module.exports = {
                             "bankName": "HSBC",
                             "accountType": "PCA",
                             "accountTitle": "HSBC Advance bank Account",
-                            "contributingAmount": '2,000',
-                            "totalAvailableBalance": '3,200',
-                            "beforeContributingBalance": '3,200',
-                            "afterContributingBalance": '1,200',
+                            "contributingAmount": '800',
+                            "totalAvailableBalance": '2,700',
+                            "beforeContributingBalance": '2,700',
+                            "afterContributingBalance": '1,900',
                             "interestRate": 0.1
                         }
                     ]
                 },
                 {
-                    bankName: 'BOS',
+                    bankName: 'RBS',
                     accountType: 'CC',
-                    accountTitle: 'Classic Credit Card',
+                    accountTitle: 'Reward Black Credit Card',
                     totalBalanceDue: '2,000',
                     clearedTotalDue: true,
                     apr: 27.10,
-                    totalOutstandingBalance: 0,
+                    totalOutstandingBalance: '0',
                     senders: [
                         {
                             "bankName": "HSBC",
-                            "aaccountType": "PCA",
-                            "accountTitle": "HSBC Advance Bank Account",
-                            "contributingAmount": '1,200',
-                            "totalAvailableBalance": '3,200',
-                            "beforeContributingBalance": '1,200',
-                            "afterContributingBalance": 0,
+                            "accountType": "PCA",
+                            "accountTitle": "HSBC Advance bank Account",
+                            "contributingAmount": '1,900',
+                            "totalAvailableBalance": '2,700',
+                            "beforeContributingBalance": '1,900',
+                            "afterContributingBalance": '0',
                             "interestRate": 0.1
                         },
                         {
                             "bankName": "Halifax",
                             "accountType": "SB",
                             "accountTitle": "Every Day Saver",
-                            "contributingAmount": 800,
-                            "totalAvailableBalance": '1,000',
-                            "beforeContributingBalance": '1,000',
-                            "afterContributingBalance": 200,
+                            "contributingAmount": 100,
+                            "totalAvailableBalance": '1,900',
+                            "beforeContributingBalance": '1,900',
+                            "afterContributingBalance": '1,800',
                             "interestRate": 0.2 
                         }
                     ]
                 },
                 {
-                    bankName: 'JPMC',
+                    bankName: 'NatWest',
                     accountType: 'CC',
-                    accountTitle: 'Freedom Unlimited',
-                    totalBalanceDue: '2,800',
+                    accountTitle: 'Reward Credit Card',
+                    totalBalanceDue: '2,900',
                     clearedTotalDue: false,
-                    apr: 25.49,
-                    totalOutstandingBalance: '1,500',
+                    apr: 23.7,
+                    totalOutstandingBalance: 900,
                     senders: [
                         {
                             "bankName": "Halifax",
                             "accountType": "SB",
                             "accountTitle": "Every Day Saver",
-                            "contributingAmount": 200,
-                            "totalAvailableBalance": '1,000',
-                            "beforeContributingBalance": 200,
-                            "afterContributingBalance": 0,
+                            "contributingAmount": '1,800',
+                            "totalAvailableBalance": '1,900',
+                            "beforeContributingBalance": '1,800',
+                            "afterContributingBalance": '0',
                             "interestRate": 0.2 
                         },
                         {
                             "bankName": "Barclays",
-                            "aaccountType": "SB",
+                            "accountType": "SB",
                             "accountTitle": "Easy Saver",
-                            "contributingAmount": '1,100',
-                            "totalAvailableBalance": '1,100',
-                            "beforeContributingBalance": '1,100',
+                            "contributingAmount": 200,
+                            "totalAvailableBalance": '200',
+                            "beforeContributingBalance": '200',
                             "afterContributingBalance": 0,
-                            "interestRate": 0.25
+                            "interestRate": 0.25 
                         }
                     ]
                 }
