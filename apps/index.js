82cbe323d5895f581eb5a62ec16eb4369222e625
Gajendra Singh <gsingh@contractor.ea.com>
2018-11-21 19:45:42 +0530

    Added methods for analysis of savings

---
+++ apps/index.js
@@ -0,0 +1,30 @@
+import { getSimpleInterest, getCompundInterest } from '../lib';
+
+// Calculate SI for all debit accounts
+let calcSIForDebit = (banks) => {
+    let totalSI = banks.reduce((si, bank) => {
+        let { accounts } = bank;
+        return si + getSimpleInterest(accounts[0].availableBalance, accounts[0].interestRate)
+    }, 0)
+
+    return totalSI;
+}
+
+// Calculate CI for all credit accounts
+let calcCIForCredit = (banks) => {
+    let totalCI = banks.reduce((ci, bank) => {
+        let { accounts } = bank;
+        return ci + getCompundInterest(accounts[0].totalBalanceDue, accounts[0].apr)
+    }, 0)
+
+    return totalCI;
+}
+
+// Calculate difference between SI of debit accounts and CI of credit accounts monthly.
+let calcDiff = (debitBanks, creditBanks) => {
+    return Math.abs(calcSIForDebit(debitBanks) - calcCIForCredit(creditBanks));
+}
+
+module.exports = {
+    calcDiff
+}
